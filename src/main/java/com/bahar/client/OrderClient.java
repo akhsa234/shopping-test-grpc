6b28e18dd4181a29584f7b9966111cd6f9ceaf32
akhsa234 <saremi.bah2000@gmail.com>
2022-08-31 17:03:48 +0430

    third commit  client package

--- src/main/java/com/bahar/client/OrderClient.java
+++ src/main/java/com/bahar/client/OrderClient.java
@@ -1,4 +1,28 @@
 package com.bahar.client;
 
+import com.shopping.stubs.order.Order;
+import com.shopping.stubs.order.OrderRequest;
+import com.shopping.stubs.order.OrderResponse;
+import com.shopping.stubs.order.OrderServiceGrpc;
+import io.grpc.Channel;
+
+import java.util.List;
+
 public class OrderClient {
+    // get a stub obj
+    //call service method
+
+    private final OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
+
+    public OrderClient(Channel channel) {
+
+        this.orderServiceBlockingStub = OrderServiceGrpc.newBlockingStub(channel);
+    }
+
+    public List<Order> getOrders(int userId) {
+
+        OrderRequest orderRequest = OrderRequest.newBuilder().setUserId(userId).build();
+        OrderResponse orderResponse = orderServiceBlockingStub.getOrdersForUser(orderRequest);
+        return orderResponse.getOrderList();
+    }
 }
